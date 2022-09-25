73b9fe617811f85c3704d9044bbc70f030c601f0
abdelrhman ashraf <mido59478@gmail.com>
2022-09-25 16:02:00 +0200

    first time

---
+++ qjava.js
@@ -0,0 +1,5 @@
+// change mode with button up
+let ButtonMode = document.getElementById("mode-btn");
+ButtonMode.addEventListener('click', () => {
+    document.body.classList.toggle("darkMode");
+})
